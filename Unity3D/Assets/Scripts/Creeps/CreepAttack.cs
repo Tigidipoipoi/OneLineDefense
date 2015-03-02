00eb281d3df07cadb6c48b3ca3e29e56f9da9267
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-03-02 15:32:01 +0100

    Navmesh WIP

---
+++ Unity3D/Assets/Scripts/Creeps/CreepAttack.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreepAttack : MonoBehaviour {
+    #region Members
+    // Area of aggro
+    public SphereCollider mAOA;
+    CreepScript mCreepScript;
+    #endregion
+
+    void Awake () {
+        mCreepScript = this.GetComponent<CreepScript> ();
+    }
+
+    void Start () {
+        mAOA.radius = mCreepScript.mCreepStats.mAttribute.mRange;
+    }
+}
---
+++ Unity3D/Assets/Scripts/Creeps/CreepMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreepMovement : MonoBehaviour {
+    #region Members
+    Vector3 mEnemyBasePos;
+    NavMeshAgent mNav;
+    #endregion
+
+    void Awake () {
+        mNav = this.GetComponent<NavMeshAgent> ();
+    }
+
+    void Start () {
+        mEnemyBasePos = GameObject.Find ("EnemyBase").transform.position;
+        mNav.SetDestination (mEnemyBasePos);
+    }
+}
