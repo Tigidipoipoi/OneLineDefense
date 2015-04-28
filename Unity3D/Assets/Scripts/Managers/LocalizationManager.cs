3725c1dadf80d2347f0f5a12e9589f853b8b5ca4
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-04-28 17:04:28 +0200

    Start Localization

---
+++ Unity3D/Assets/Scripts/Managers/LocalizationManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocalizationManager : MonoBehaviour {
+    #region Singleton
+    static private LocalizationManager s_Instance;
+    static public LocalizationManager GetInstance {
+        get {
+            return s_Instance;
+        }
+    }
+
+    void Awake() {
+        if (s_Instance == null) {
+            s_Instance = this;
+        }
+        DontDestroyOnLoad(this);
+        Init();
+    }
+    #endregion
+
+    public TextAsset m_CSVFile;
+
+    public void Init() {
+        // Parse: m_CSVFile.text;
+    }
+}
