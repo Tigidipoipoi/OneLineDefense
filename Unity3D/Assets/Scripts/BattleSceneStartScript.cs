228d74f39a0b8da99539f898f4bf605aa94a5d3e
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-05-15 11:31:32 +0200

    Adds SceneAutoLoader

--- Unity3D/Assets/Scripts/BattleSceneStartScript.cs
+++ Unity3D/Assets/Scripts/BattleSceneStartScript.cs
@@ -4,5 +4,6 @@ using System.Collections;
 public class BattleSceneStartScript : MonoBehaviour {
     public void Start() {
         ResourcesManager.GetInstance.Init();
+        CreepManager.GetInstance.FindAllySpawn();
     }
 }
